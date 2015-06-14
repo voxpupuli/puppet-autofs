6954555ee258c225318a3baf648a38f9fddad2f2
David Hollinger III <DavidHollinger@solutionary.com>
2015-06-14 16:25:48 -0500

    completed package test. modified spec_helper and init test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 describe 'autofs', :type => :class do
 
   it { should contain_class('autofs::package') }
-  it { should contain_class('autofs::config') }
-  it { should contain_class('autofs::service') }
+#  it { should contain_class('autofs::config') }
+#  it { should contain_class('autofs::service') }
 
 end
