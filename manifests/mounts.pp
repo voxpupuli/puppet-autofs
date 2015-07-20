d8bda09613d7357a7d380e544150831e9309d3aa
David Hollinger III <david.hollinger@moduletux.com>
2015-07-19 22:22:41 -0500

    moved concat code from config to mounts

--- manifests/mounts.pp
+++ manifests/mounts.pp
@@ -1,5 +1,12 @@
 # Class: autofs::mounts
 #
 class autofs::mounts( $mount ){
+  concat { '/etc/auto.master':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    notify => Service[ 'autofs' ],
+  }
+  
   create_resources('autofs::mount', $mount)
 }
