2db46aea23d3360ab030301631f01ce1d0d576cf
David Hollinger III <david.hollinger@moduletux.com>
2015-07-19 21:50:33 -0500

    added more mount define documentation

--- manifests/mount.pp
+++ manifests/mount.pp
@@ -14,7 +14,16 @@
 # [*mapcontents*]
 #  The mount point options and parameters,
 #  Example: '* -user,rw,soft server.example.com:/path/to/home/shares'
-
+#
+# [*options*]
+#  Options for the autofs mount point within in the auto.master
+#
+# [*order*]
+#  Order in which entries will appear in the autofs master file.
+#
+# These Parameters can be set statically,  within an ENC, or by using Hiera.
+# See README Docs for Examples.
+#
 define autofs::mount (
   $mount,
   $mapfile,
