746248dea51974ecba7f5e1779a840b053ffa366
David Hollinger III <david.hollinger@moduletux.com>
2015-07-19 23:03:18 -0500

    fixed config spec test

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe 'autofs::config', :type => :class do
   it do
-    should contain_file('master_file').with_ensure('file').with_path('/etc/auto.master')
+    should contain_concat('/etc/auto.master')
   end
 end
