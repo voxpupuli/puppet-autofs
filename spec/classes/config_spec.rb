100148bb33d080fabaa5345efde70b675ce3e572
David Hollinger III <david.hollinger@moduletux.com>
2015-07-19 23:04:40 -0500

    more spec test updates

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 describe 'autofs::config', :type => :class do
   it do
+    let(:facts) do
+      {
+        :concat_basedir => '/etc'
+      }
+    end
     should contain_concat('/etc/auto.master')
   end
 end
