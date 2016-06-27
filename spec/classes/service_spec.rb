71de0f52988af2cf0c84de64d58e1b663e8a28af
David Hollinger III <david.hollinger@egov.com>
2016-06-27 16:56:24 -0500

    Update service class tests

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -2,15 +2,7 @@ require 'spec_helper'
 describe 'autofs::service', :type => :class do
 
   context 'test default service' do
-    let(:params) do
-      {
-          :service_ensure => 'running',
-          :service_enable => 'true'
-      }
-    end
     it { should contain_service('autofs').with(
-                    'ensure' => :service_ensure,
-                    'enable' => :service_enable,
                     'hasstatus' => 'true',
                     'hasrestart' => 'true',
                 )
