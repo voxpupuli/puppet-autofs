9df0384b562f4f9bff16701752b1f98f1c76dd07
David Hollinger III <david.hollinger@moduletux.com>
2016-11-26 23:57:04 -0600

    Add puppet-strings documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,12 +9,18 @@
 #
 # === Authors
 #
-# David Hollinger III <EagleDelta2@gmail.com>
+# David Hollinger III <david.hollinger@moduletux.com>
 #
 # === Copyright
 #
 # Copyright 2014 David Hollinger III
 #
+# @example Declaring the autofs class
+#    include autofs
+#
+# @param mounts hash parameter containing autofs config data
+# @param use_map_dir boolean parameter determining the use of auto.master.d
+# @param map_dir string parameter used to set the directory for use_map_dir
 #
 class autofs (
   Hash $mounts         = {},
