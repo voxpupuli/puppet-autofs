a522731f569965f972498905062df3310abeba1c
David Hollinger III <david.hollinger@moduletux.com>
2016-11-26 23:57:34 -0600

    Add puppet-strings documentation

--- manifests/mounts.pp
+++ manifests/mounts.pp
@@ -1,6 +1,23 @@
 # Class: autofs::mounts
 #
-class autofs::mounts () {
+# Class used specifically to pass hiera autolookups as a hiera_hash
+# to the autofs::mount defined type.
+#
+# @example using autofs::mounts class with hiera
+#    ---
+#    autofs::mounts:
+#      home:
+#        mount: '/home'
+#        mapfile: '/etc/auto.home'
+#        mapcontents:
+#          - '* -user,rw,soft,intr,rsize=32768,wsize=32768,tcp,nfsvers=3,noacl server.example.com:/path/to/home/shares'
+#        options: '--timeout=120'
+#        order: 01
+#
+# @param mount Passes hiera values as a hash to the autofs::mount defined type
+#   using the create_resources function.
+#
+class autofs::mounts() {
   $mount = hiera_hash('autofs::mounts', [])
 
   create_resources('autofs::mount', $mount)
