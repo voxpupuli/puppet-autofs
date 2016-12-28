1641e135ee5bb43aab24f6e89f818480a4b5f8a9
David Hollinger III <david.hollinger@egov.com>
2016-12-27 21:40:05 -0600

    Update spec_helper_acceptance for new beaker version

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -8,11 +8,9 @@ begin
 rescue LoadError
 end
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-
 RSpec.configure do |c|
   # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
   # Readable test descriptions
   c.formatter = :documentation
@@ -26,13 +24,15 @@ RSpec.configure do |c|
   c.before :suite do
     # Install module and deps
     hosts.each do |host|
-      copy_module_to(host, :source => proj_root, :module_name => 'autofs')
+      install_puppet_agent_on(hosts, options)
+      install_dev_puppet_module_on(host, :source => module_root, :module_name => 'autofs',
+          :target_module_path => '/opt/puppetlabs/puppet/modules')
       # Due to RE-6764, running yum update renders the machine unable to install
       # other software. Thus this workaround. - thanks to Gareth Rushgrove's work for this
-      if fact_on(host, 'operatingsystem') == 'RedHat'
-        on(host, 'mv /etc/yum.repos.d/redhat.repo /etc/yum.repos.d/internal-mirror.repo')
-      end
-      on(host, 'yum update -y -q') if fact_on(host, 'osfamily') == 'RedHat'
+      #if fact_on(host, 'operatingsystem') == 'RedHat'
+      #  on(host, 'mv /etc/yum.repos.d/redhat.repo /etc/yum.repos.d/internal-mirror.repo')
+      #end
+      #on(host, 'yum update -y -q') if fact_on(host, 'osfamily') == 'RedHat'
 
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1]}
       on host, puppet('module', 'install', 'puppetlabs-concat'), { :acceptable_exit_codes => [0,1]}
