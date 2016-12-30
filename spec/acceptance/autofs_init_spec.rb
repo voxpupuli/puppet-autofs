0a196aa85024cbca8d99e8ed83e0c71f78361178
David Hollinger III <david.hollinger@egov.com>
2016-12-29 23:13:52 -0600

    Add tests for auto.master.d option

--- spec/acceptance/autofs_init_spec.rb
+++ spec/acceptance/autofs_init_spec.rb
@@ -24,7 +24,7 @@ describe 'autofs' do
           }
         EOS
 
-        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, catch_failures: true)
       end
 
       describe file('/etc/auto.master') do
@@ -58,5 +58,66 @@ describe 'autofs' do
         it { is_expected.to be_running }
       end
     end
+
+    context 'master directory test' do
+      it 'applies' do
+        pp = <<-EOS
+          class { 'autofs': }
+          autofs::mount { 'confdir':
+            mount       => '/mnt/confdir',
+            mapfile     => '/etc/auto.confdir',
+            mapcontents => ['test_dir -o rw /mnt/test_conf'],
+            options     => '--timeout=120',
+            order       => 02,
+            use_dir     => true,
+          }
+        EOS
+
+        apply_manifest(pp, catch_failures: true)
+      end
+
+      describe file('/etc/auto.master') do
+        it 'should exist and have content' do
+          is_expected.to exist
+          is_expected.to be_owned_by 'root'
+          is_expected.to be_grouped_into 'root'
+          shell('cat /etc/auto.master') do |s|
+            expect(s.stdout).to match(/\+dir\:\/etc\/auto.master.d/)
+        end
+      end
+
+      describe file('/etc/auto.master.d') do
+        it 'should be a directory owned by root' do
+          is_expected.to be_directory
+          is_expected.to be_owned_by 'root'
+          is_expected.to be_grouped_into 'root'
+        end
+      end
+
+      describe file('/etc/auto.master.d/confdir.autofs') do
+        it 'should exist and have content' do
+          is_expected.to exist
+          is_expected.to be_owned_by 'root'
+          is_expected.to be_grouped_into 'root'
+          shell('cat /etc/auto.master.d/confdir.autofs') do |s|
+            expect(s.stdout).to match(/\/mnt\/confdir \/etc\/auto.confdir --timeout=120/)
+          end
+        end
+      end
+
+      describe file('/etc/auto.confdir') do
+        it 'should exist and have content' do
+          is_expected.to exist
+          is_expected.to be_owned_by 'root'
+          is_expected.to be_grouped_into 'root'
+          shell('cat /etc/auto.confdir') do |s|
+            expect(s.stdout).to match(/test_dir -o rw \/mnt\/test_conf/)
+          end
+        end
+      end
+
+      end
+    end
+
   end
 end
