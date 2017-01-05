1ee630e4314f584148dbf8f52a4d5313981743fb
David Hollinger III <david.hollinger@egov.com>
2017-01-05 16:36:07 -0600

    Add puppet-strings gem

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@
 # file names.
 #
 # @see https://dhollinger.github.io/autofs-puppet Home
-# @see https://www.github.com/dhollinger/autofs-puppet Github
-# @see https://forge.puppet.com/dhollinger/autofs Puppet Forge
+# @see https://www.github.com/voxpupuli/autofs-puppet Github
+# @see https://forge.puppet.com/puppet/autofs Puppet Forge
 #
 # @author David Hollinger III <david.hollinger@moduletux.com>
 #
