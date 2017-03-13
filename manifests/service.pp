ad31fb8bae229d530d5f811edc087643c793a359
David Hollinger III <david.hollinger@moduletux.com>
2017-03-13 00:29:03 -0500

    Update autofs service to reference new parameters

--- manifests/service.pp
+++ manifests/service.pp
@@ -13,26 +13,12 @@
 # @author Vox Pupuli <voxpupuli@groups.io>
 # @author David Hollinger III <david.hollinger@moduletux.com>
 #
-# @param ensure Determines state of the service. Can be set to: running or stopped.
-# @param enable Determines if the service should start with the system boot. true
-#   will start the autofs service on boot. false will not start the autofs service
-#   on boot.
-# @param service_restart Determines if the service has a restart command. If true,
-#   puppet will use the restart command to restart the service. If false, the
-#   stop, then start commands will be used instead.
-# @param service_status Determines if service has a status command.
-#
-class autofs::service (
-  String $ensure           = running,
-  Boolean $enable          = true,
-  Boolean $service_restart = true,
-  Boolean $service_status  = true
-){
+class autofs::service {
   service { 'autofs':
-    ensure     => $ensure,
-    enable     => $enable,
-    hasstatus  => $service_status,
-    hasrestart => $service_restart,
+    ensure     => $autofs::service_ensure,
+    enable     => $autofs::service_enable,
+    hasstatus  => $autofs::service_status,
+    hasrestart => $autofs::service_restart,
     require    => Package['autofs'],
   }
 }
