a7a71e0f5bf7f084b33f21d361450acd6899b50b
David Hollinger III <david.hollinger@moduletux.com>
2017-03-19 15:56:04 -0500

    Add new fact for tracking autofs version

---
+++ lib/facter/autofs.rb
@@ -0,0 +1,9 @@
+Facter.add(:autofs_version) do
+  setcode do
+    if Facter::Util::Resolution.which('automount')
+      autofs_version_command = 'automount -V 2>&1'
+      autofs_version = Facter::Util::Resolution.exec(autofs_version_command)
+      %r{Linux automount version ([\w\.]+)}.match(autofs_version)[1]
+    end
+  end
+end
---
+++ spec/unit/autofs_fact_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Facter::Util::Fact do
+  before { Facter.clear }
+  after { Facter.clear}
+
+  context 'autofs not in path' do
+    before do
+      Facter::Util::Resolution.stubs(:which).with('automount').returns(false)
+    end
+    it { expect(Facter.fact(:autofs_version).value).to eq(nil) }
+  end
+  context 'autofs' do
+    before do
+      Facter::Util::Resolution.stubs(:which).with('automount').returns(true)
+      Facter::Util::Resolution.stubs(:exec).with('automount -V 2>&1').returns('Linux automount version 5.1.1')
+    end
+    it { expect(Facter.fact(:autofs_version).value).to eq('5.1.1')}
+  end
+end
