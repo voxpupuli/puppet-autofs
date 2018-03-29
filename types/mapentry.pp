7f734b5b006f747e7c9b23806e3cc860eb288384
John Bollinger <John.Bollinger@StJude.org>
2018-03-29 10:57:03 -0500

    Made the Autofs::Mapentry type more permissive
    The Autofs::Mapentry type rejected some map designations that Autofs itself
    permits.  The type is updated to accept all valid designations that
    aren't plain absolute paths.
    
    Inasmuch as the accepted designations vary with the specified map type,
    and this data type (still) does not make distinctions based on map type,
    it now accepts some map designations that Autofs will reject.  Specifically,
    for 'file', 'directory', and 'program' map types, Autofs expects the map name
    to be an absolute path, and the Autofs::Mapentry data type no longer
    enforces this.

--- types/mapentry.pp
+++ types/mapentry.pp
@@ -1,6 +1,9 @@
-# This type matches a map type and path.
+# This type matches a map specfication with map type and optional format,
+# or the built-in -hosts map.
 # @example program:/etc/auto.smb
 # @example file:/etc/auto.file
-# @example hosts:-hosts
+# @example file,sun:/etc/auto.file
+# @example yp:mnt.map
+# @example -hosts
 
-type Autofs::Mapentry = Pattern[/^[a-z]+:\/([^\/\0]+(\/)?)+$|^-hosts$/]
+type Autofs::Mapentry = Pattern[/\A([a-z]+(,[a-z]+)?:\S+|-hosts)\z/]
