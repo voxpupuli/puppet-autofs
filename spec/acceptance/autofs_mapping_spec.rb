618bbcac4a6f2fcac556869ddf548d41788111e5
George Stagg <george.stagg@newcastle.ac.uk>
2020-04-07 14:49:44 +0000

    Add acceptance tests for autofs::mapping

---
+++ spec/acceptance/autofs_mapping_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper_acceptance'
+
+describe 'autofs::mapping tests' do
+  context 'basic mapping test' do
+    it 'applies' do
+      pp = <<-EOS
+        autofs::mapfile { '/etc/auto.data':
+        }
+        autofs::mapping { 'dataA':
+          mapfile => '/etc/auto.data',
+          key     => 'dataA',
+          options => 'ro',
+          fs      => 'fs.net:/export/dataA',
+        }
+      EOS
+
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe file('/etc/auto.data') do
+      it 'exists and belongs to root' do
+        is_expected.to exist
+        is_expected.to be_owned_by 'root'
+        is_expected.to be_grouped_into 'root'
+      end
+      its(:content) do
+        is_expected.to match %r{^\s*dataA\s+-ro\s+fs.net:/export/dataA\s*$}
+      end
+    end
+  end
+
+  context 'with order parameter' do
+    it 'applies' do
+      pp = <<-EOS
+        autofs::mapfile { '/etc/auto.data':
+          mappings => [
+            { 'key' => 'dataB', 'options' => 'rw,noexec', 'fs' => 'fs.net:/export/dataB', 'order' => 20 },
+          ]
+        }
+        autofs::mapping { 'dataA':
+          mapfile => '/etc/auto.data',
+          key     => 'dataA',
+          options => 'ro',
+          fs      => 'fs.net:/export/dataA',
+          order   => 10,
+        }
+      EOS
+
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe file('/etc/auto.data') do
+      it 'exists and belongs to root' do
+        is_expected.to exist
+        is_expected.to be_owned_by 'root'
+        is_expected.to be_grouped_into 'root'
+      end
+      its(:content) do
+        is_expected.to match %r{^\s*dataA\s+-ro\s+fs.net:/export/dataA\s*\n\s*dataB\s+-rw,noexec\s+fs.net:/export/dataB\s*$}
+      end
+    end
+  end
+end
